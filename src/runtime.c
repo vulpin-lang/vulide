b4a6b520078ccbd3074645e717d4fb6e57f2ccc6
BatScript <124243547+Bat-Script@users.noreply.github.com>
2026-07-17 14:33:46 +0330

    Update runtime.c

--- src/runtime.c
+++ src/runtime.c
@@ -1,19 +1,12 @@
+// 2026 COPYRIGHT BY "VULPIN SOFTWARE.CC" ALL RIGHT REVERSED.
+// MIT LICENCE
+
 #include <stdio.h>
 #include <string.h>
-
-// Copyright 2026 BatScript
-// ------
-// To compile into a .so:
-// gcc -shared -fPIC -o runtime.so runtime.c
-// ------
-// In Python:
-// vulpin_rt = ctypes.CDLL('./runtime.so')
-// 
 
 void run_vulpin(const char *filename, char *output_buffer, int buffer_size) {
     char command[512];
     snprintf(command, sizeof(command), "python vul.py %s 2>&1", filename);
-
     FILE *fp = popen(command, "r");
     if (fp != NULL) {
         // Read the output and put it into the buffer send to Python
