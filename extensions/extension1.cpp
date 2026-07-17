74cbb3251411d68c77a8de2d44a1f89af338f602
BatScript <124243547+Bat-Script@users.noreply.github.com>
2026-07-17 18:00:57 +0330

    Update extension1.cpp

--- extensions/extension1.cpp
+++ extensions/extension1.cpp
@@ -1,3 +1,3 @@
 // Extensions are packages which User can install them by importing extensions to extensions folder
-// This is just a note file ``extension1.py```
+// This is just a note file ``extension1.cpp```
 // we might make a sample on this file.
