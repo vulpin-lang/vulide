c5440b5daaf15a7e94fc078a6c34a6397490dfcf
BatScript <124243547+Bat-Script@users.noreply.github.com>
2026-07-17 17:59:41 +0330

    Update and rename extension1.py to extension1.cpp

---
+++ extensions/extension1.cpp
@@ -0,0 +1,3 @@
+// Extensions are packages which User can install them by importing extensions to extensions folder
+// This is just a note file ``extension1.py```
+// we might make a sample on this file.
