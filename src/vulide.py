74bc9b1f46d6c24481ad7cad486fa6902b21b0bb
stefpga <stefandragan95@gmail.com>
2026-07-17 13:13:00 +0300

    Integrate C function call and Tkinter output display
    Added functionality to call a C function from Python using ctypes and display output in a Tkinter UI.

--- src/vulide.py
+++ src/vulide.py
@@ -1,4 +1,7 @@
 import subprocess
+import ctypes
+import tkinter as tk
+
 
 result = subprocess.run(
     ["./main"],
@@ -8,3 +11,22 @@ result = subprocess.run(
 )
 
 print(result.stdout)
+
+
+vulpin_lib = ctypes.CDLL('./libvulpin.so')
+# specify the args for all upcoming C functions.
+vulpin_lib.run_vulpin.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int]
+
+def on_run_button_click():
+    filename = "test.vul" # FIX SO IT GETS FROM UI
+    
+    # Create a buffer for the C code to fill
+    buffer = ctypes.create_string_buffer(2048)
+    
+    # Call the C function
+    vulpin_lib.run_vulpin(filename.encode('utf-8'), buffer, 2048)
+    
+    # Put the result into Tkinter widget
+    output = buffer.value.decode('utf-8')
+    console.insert("end", output)
+
