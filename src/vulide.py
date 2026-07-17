6d097613c7619815f83ac53c7f66bda59913c669
BatScript <124243547+Bat-Script@users.noreply.github.com>
2026-07-17 11:37:35 +0330

    Update vulide.py

--- src/vulide.py
+++ src/vulide.py
@@ -1,3 +1,10 @@
-import subprocess
+#include <stdio.h>
 
-subprocess.run(["./app", "Hello from Python!"])
+int main() {
+    char text[100];
+
+    scanf("%99s", text);        // Read from Python
+    printf("Hello %s\n", text); // Send back to Python
+
+    return 0;
+}
