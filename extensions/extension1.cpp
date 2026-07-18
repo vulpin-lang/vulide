9b194f5e7b2f8b22e1399c4136c8af292501f6ed
BatScript <124243547+Bat-Script@users.noreply.github.com>
2026-07-18 11:29:33 +0330

    Delete extensions/extension1.cpp

--- extensions/extension1.cpp
+++
@@ -1,3 +0,0 @@
-// Extensions are packages which User can install them by importing extensions to extensions folder
-// This is just a note file ``extension1.cpp```
-// we might make a sample on this file.
